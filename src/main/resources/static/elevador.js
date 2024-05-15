2ec1b02d3a6c812b9b5a9dfd390bb4266c9d1a92
lopesasuos <lopes.asuos@gmail.com>
2024-05-15 15:28:27 -0300

    Adição de Case 2

---
+++ src/main/resources/static/elevador.js
@@ -0,0 +1,59 @@
+let elevador = document.getElementById('elevador');
+let display = document.getElementById('display');
+let andares = document.querySelectorAll('.andar');
+let elevadorPosicao = 0;
+
+function fecharPortas() {
+  elevador.classList.remove('aberto');
+  elevador.classList.add('fechado');
+  atualizarDisplay(elevadorPosicao, false); // Passa false para indicar que as portas estão fechadas
+}
+
+function abrirPortas() {
+  elevador.classList.remove('fechado');
+  elevador.classList.add('aberto');
+  atualizarDisplay(elevadorPosicao, true); // Passa true para indicar que as portas estão abertas
+}
+
+function atualizarDisplay(andar, estadoPortas) {
+  let andarTexto = andar === 0 ? 'Térreo' : `${andar}º`;
+  let estadoTexto = estadoPortas ? '| aberto' : '| fechado';
+  display.innerText = `${andarTexto} ${estadoTexto}`;
+}
+
+function atualizarBotaoSelecionado(andar) {
+  andares.forEach(botao => botao.classList.remove('selecionado'));
+  const botaoSelecionado = document.querySelector(`.andar[data-andar="${andar}"]`);
+  botaoSelecionado.classList.add('selecionado');
+}
+
+function moverElevador(andarDestino) {
+  fecharPortas();
+  atualizarDisplay(elevadorPosicao);
+  atualizarBotaoSelecionado(elevadorPosicao);
+
+  let intervalo = setInterval(() => {
+    if (elevadorPosicao < andarDestino) {
+      elevadorPosicao++;
+    } else if (elevadorPosicao > andarDestino) {
+      elevadorPosicao--;
+    } else {
+      clearInterval(intervalo);
+      abrirPortas();
+      atualizarBotaoSelecionado(andarDestino);
+      return;
+    }
+
+    atualizarDisplay(elevadorPosicao);
+    atualizarBotaoSelecionado(elevadorPosicao);
+  }, 1000);
+}
+
+function selecionarAndar(event) {
+  let andarSelecionado = parseInt(event.target.getAttribute('data-andar'));
+  moverElevador(andarSelecionado);
+}
+
+andares.forEach(andar => {
+  andar.addEventListener('click', selecionarAndar);
+});
